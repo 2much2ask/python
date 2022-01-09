b64b4782b7ddda819cd77f4a835a0ccdd20d1bae
Bella <bella.bedoshvili@gmail.com>
2022-01-10 01:23:48 +0300

    added tasks 1,4,5

---
+++ lesson_2/lesson_2_task_5.py
@@ -0,0 +1,18 @@
+prices = [22.5, 15.87, 120, 10.03, 19.99, 18.98, 5, 0.15, 17, 13.5]
+price_list = []
+for i in prices:
+    roubles = int(i)
+    kop = int((i - int(i)) * 100)
+    item = f"{roubles} руб. {kop:02d} коп"
+    price_list.append(item)
+print(', '.join(price_list))
+
+# сортировка по возрастанию
+print(id(prices))   # до сортировки
+prices.sort()
+print(prices, id(prices))   # после сортировки
+# сортировка по убыванию
+prices_sorted = sorted(prices, reverse=True)
+print(prices_sorted, id(prices_sorted))
+# по возрастанию
+print(prices[:5])
