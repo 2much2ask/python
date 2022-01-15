7123266b0f5b3ed1e0af984094871d60fc1cdfba
Bella <bella.bedoshvili@gmail.com>
2022-01-16 02:07:06 +0300

    added hw files 2 and 3(2*)

---
+++ lesson_2/lesson_2_task_2.py
@@ -0,0 +1,14 @@
+sentence = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+new_sentence = []
+for el in sentence:
+    if el.isdigit():
+        new_sentence.append(f'"{el.zfill(2)}"')
+    elif el[1:].isdigit() or el[0] == '+' or el[0] == '-':
+        new_sentence.append(f'"{el[0] + el[1:].zfill(2)}"')
+    else:
+        new_sentence.append(el)
+print(' '.join(new_sentence))
+
+
+
