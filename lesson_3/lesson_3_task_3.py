c172ed10e5b7e15f662d6d74a298368e1f5532c2
Bella <bella.bedoshvili@gmail.com>
2022-01-23 15:33:08 +0300

    corrected mistake - deleted empty line

--- lesson_3/lesson_3_task_3.py
+++ lesson_3/lesson_3_task_3.py
@@ -1,10 +1,11 @@
 def thesaurus(*names):
-    alphabet = ['AБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ']
     names_dict = {}
     for name in names:
-        names_dict[name[0]] = name
+        if names_dict.get(name[0]) is None:
+            names_dict[name[0]] = [name]
+        else:
+            names_dict[name[0]].append(name)
     return names_dict
-
 
 
 print(thesaurus("Иван", "Мария", "Петр", "Илья", "Павел", "Элина"))
--- lesson_3/lesson_3_task_5.py
+++ lesson_3/lesson_3_task_5.py
@@ -1,21 +1,30 @@
-from random import choice
+import random
+from random import choice, sample
 
-nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-
-
-def get_jokes(n):
+def get_jokes(n, repeat=True):
     """Returns n jokes with random words from the lists"""
+
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
     joke_list = []
-    a = 1
-    while a <= n:
-        joke = choice(nouns) + ' ' + choice(adverbs) + ' ' + choice(adjectives)
-        joke_list.append(joke)
-        a += 1
+    a = 0
+    if repeat:
+        while a < n:
+            joke = choice(nouns) + ' ' + choice(adverbs) + ' ' + choice(adjectives)
+            joke_list.append(joke)
+            a += 1
+    else:
+        noun = random.sample(nouns, n)
+        adverb = random.sample(adverbs, n)
+        adjective = random.sample(adjectives, n)
+        while a < n:
+            joke = noun[a] + ' ' + adverb[a] + ' ' + adjective[a]
+            joke_list.append(joke)
+            a += 1
     return joke_list
 
 
 print(get_jokes(4))
-
-# print(get_jokes(input('введите число шуток, которые вам нужны ')))
+#print(get_jokes(int(input('введите число шуток, которые вам нужны '))))
