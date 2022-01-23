3e69d7fef02c10b3dc83a26f1cf05341e7ffbf92
Bella <bella.bedoshvili@gmail.com>
2022-01-23 15:33:08 +0300

    corrected mistake - deleted empty line

--- lesson_3/lesson_3_task_4.py
+++ lesson_3/lesson_3_task_4.py
@@ -1,10 +1,14 @@
-def thesaurus(*names):
-    alphabet = ['AБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ']
+def thesaurus_adv(*names):
     names_dict = {}
     for name in names:
-        names_dict[name[0]] = name
+        name_first_letter, surname_first_letter = name[0], name[name.find(' ')+1]
+        if surname_first_letter not in names_dict:
+            names_dict[surname_first_letter] = {name_first_letter: [name]}
+        elif name_first_letter in names_dict[surname_first_letter]:
+            names_dict[surname_first_letter][name_first_letter].append(name)
+        else:
+            names_dict[surname_first_letter][name_first_letter] = [name]
     return names_dict
 
 
-
-print(thesaurus("Иван", "Мария", "Петр", "Илья", "Павел", "Элина"))
+print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
