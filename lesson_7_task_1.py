369e1ee746aed2e82c1e8763ddf876f055b04db6
Bella <bella.bedoshvili@gmail.com>
2022-04-12 12:32:27 +0300

    удалила лишние

--- lesson_7_task_1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-root_dir = 'my_project'
-folders = ['settings', 'mainapp', 'adminapp', 'authapp']
-
-for i in folders:
-    dir_path = os.path.join(root_dir, i)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path, exist_ok=True)
